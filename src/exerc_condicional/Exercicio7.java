cbb1a20ec1c89d37481edd5ab9c63c8c2a6bc7fa
Mailza <mailza.souzalima2023@gmail.com>
2024-06-05 16:47:54 -0300

    nd

--- src/exerc_condicional/Exercicio7.java
+++ src/exerc_condicional/Exercicio7.java
@@ -27,7 +27,7 @@ public class Exercicio7 {
 			System.out.print("Q4");
 		} else if (x == 0 && y > 0 || y < 0) {
 			System.out.print("Eixo Y");
-		} else if (y == 0 && x > 0.0 || x < 0.0) {
+		} else if(y == 0 && x > 0.0 || x < 0.0) {
 			System.out.print("Eixo X");
 		}
 
